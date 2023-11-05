5ac92c488dd878c0cfc4a690c9c4fbea6e78a771
Tanzim <tanzim008@gmail.com>
2023-11-05 01:55:47 -0230

    todo items displayed

--- src/components/Todo/Index.jsx
+++ src/components/Todo/Index.jsx
@@ -11,16 +11,25 @@ const Index = () => {
   return (
     <div>
       <h1>TO DO APP</h1>
-      {/* Todo form
-      tooo add button
-      todo task lists
-      todo complet function
-      show completed tasks */}
-    {/* Todo form */}
-    <div className="input-wrapper">
-        <input type="text" name='todo' placeholder='Add a new task' value={todo} onChange={(e)=>{setTodo(e.target.value)}}/>
-        <button className='add-button' onClick={addTask}>ADD TASK</button>
-    </div>
+        {/* Todo form */}
+        <div className="input-wrapper">
+            <input type="text" name='todo' placeholder='Add a new task' value={todo} onChange={(e)=>{setTodo(e.target.value)}}/>
+            <button className='add-button' onClick={addTask}>ADD TASK</button>
+        </div>
+        {/* SHOW LIST OF TASKS */}
+        {todos.length > 0 ?
+        (<ul className="todo-list">
+            {todos.map((todo,index)=>{
+                <div className="todo">
+                    <li key={index}>{todo}</li>
+                    <button className="delete-button">Delete task</button>
+                </div>
+            })}
+        </ul>)
+        :
+        (<div className="empty">
+            <p>No task to complete</p>
+        </div>)}
     </div>
   )
 }
